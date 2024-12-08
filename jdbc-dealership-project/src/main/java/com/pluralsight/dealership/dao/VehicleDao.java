bb2c4a5d2b8f0270f7500a67ae4ad09e4095dfd0
adrianvences9820 <adrianvences9820@gmail.com>
2024-12-08 13:18:06 -0500

    DealershipDao interface Class

---
+++ jdbc-dealership-project/src/main/java/com/pluralsight/dealership/dao/VehicleDao.java
@@ -0,0 +1,7 @@
+package com.pluralsight.dealership.dao;
+
+public class VehicleDao {
+
+
+
+}
---
+++ jdbc-dealership-project/src/main/java/com/pluralsight/dealership/datamanagers/DealershipDataManager.java
@@ -0,0 +1,4 @@
+package com.pluralsight.dealership.datamanagers;
+
+public class DealershipDataManager {
+}
